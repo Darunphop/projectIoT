55a147024c4654d9617c4c47ab7f5ba97c200812
chalitphol kuensuwan <chalitphol@gmail.com>
2017-11-15 17:01:06 +0700

    final

--- client1.py
+++ client1.py
@@ -26,7 +26,10 @@ def state_has_changed(text, boolean):
   return state_changed
 
 def check_state():
-    return 1 if state_has_changed("((ADC(0).read()) >= 500)", ((ADC(0).read()) >= 500)) else 0
+	return 1 if state_has_changed("(((ADC(0).read()) >= 700) and ((ADC(0).read()) <= 900))", (((ADC(0).read()) >= 700) and ((ADC(0).read()) <= 900))) else 0
+
+def check_order():
+	return 1 if state_has_changed("(((ADC(0).read()) >= 500) and ((ADC(0).read()) <= 600))", (((ADC(0).read()) >= 500) and ((ADC(0).read()) <= 600))) else 0
 
 def check_alive():
 	return 1 if state_has_changed("(((ADC(0).read()) >= 160) and ((ADC(0).read()) <= 180))", (((ADC(0).read()) >= 160) and ((ADC(0).read()) <= 180))) else 0
@@ -38,20 +41,17 @@ state = 0
 mqttms = None
 
 
-
-
-
 def main():
     global state
     global queueQ
     global queueO
     global mqttms
-    Room = 'Restaurant/1/req'
+    Room = 'Restaurant1/1/req'
     delay = 1
 
     mqtt.set_callback(onmessage)
     mqtt.connect()
-    mqtt.subscribe(b'Restaurant/1')
+    mqtt.subscribe(b'Restaurant1/1')
     while True:
       if True:
           mqtt.check_msg()
@@ -73,37 +73,35 @@ def main():
               if queueQ == "readyQ":
                   state = 2
                   time.sleep_ms(delay)
+
           if state == 2:
-              while state != 3:
                   oled.clear()
                   oled.text(str('YOUR QUEUE IS READY.'),0,0)
                   oled.text(str('READY.'),0,8)
                   oled.text(str('PLEASE PRESS'),0,16)
                   oled.text(str('THE BUTTON.'),0,24)
                   if check_state() == 1:
-                      mqtt.publish(Room,str('deqQ'),retain=False)
                       state = 3
                       time.sleep_ms(delay)
 
           if state == 3:
-              while state == 3:
                   oled.clear()
                   oled.text(str('PRESS BUTTON TO'),0,0)
                   oled.text(str('ORDER.'),0,8)
-                  if (check_state() == 1 and check_alive() != 1):
+                  if (check_order() == 1 and check_alive() != 1):
                       mqtt.publish(Room,str('reqO'),retain=False)
                       state = 4
                       time.sleep_ms(delay)
-                  elif (check_state() != 1 and check_alive() == 1):
+                  elif (check_order() != 1 and check_alive() == 1):
                       mqtt.publish(Room,str('sleep'),retain=False)
-                      state = 6
+                      state = 0
                       time.sleep_ms(delay)
 
           if state == 4:
               oled.clear()
               oled.text(str('YOUR QUEUE IS : '),0,0)
               oled.text(str(mqttms),0,8)
-              if mqttms == 0:
+              if mqttms == 1:
                   state = 5
 
           if state == 5:
@@ -111,18 +109,12 @@ def main():
                 oled.text(str('THANK YOU'),0,0)
                 oled.text(str('FOR WAITING'),0,8)
                 time.sleep_ms(2000)
-                while check_state() != 1:
+                while check_order() != 1:
                     oled.clear()
                     oled.text(str('Press Button'),0,0)
                     oled.text(str('TO ACCEPT ORDER'),0,8)
                 mqtt.publish(Room,str('deqO'),retain=False)
                 state = 3
-          if state == 6:
-                while check_state() != 1:
-                    oled.clear()
-                    oled.text(str('Press Button'),0,0)
-                    oled.text(str('To Start'),0,8)
-                mqtt.publish(Room,str('reqQ'),retain=False)
-                state = 0
+
     else:
         mqtt.wait_msg()
